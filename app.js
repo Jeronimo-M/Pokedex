f12d5ea631a1ec340c9b375fdea6aaf001f95230
Jeronimo Maranion <jeronimomaranion@MacBook-Pro-2.local>
2021-02-25 21:02:43 -0500

    MVP almost done

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// Grab Data from API
+/* ==========================================================================
+   Grab API 
+========================================================================== */
+
+// Grab individual Pokemon data 
 
 async function fetchData(pokemon) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
@@ -6,9 +10,9 @@ async function fetchData(pokemon) {
   try {
     const response = await axios.get(url)
     // console.log(response)
-    let data = response.data[0]
-    // console.log(response.data)
-    // showPokeData(data)
+    let data = response.data
+    
+    showPokeData(data)
     return response
   } catch (err) {
     console.error(err)
@@ -17,17 +21,110 @@ async function fetchData(pokemon) {
 
 // fetchData('pikachu')
 
-function showPokeData(data) {
-  
-  // console.log("inside funct", data)
-  dataContainer = document.querySelector('.left-container-screen')
-  console.log(dataContainer)
+// Grab entire list of Pokemon 
 
-  const pokeData = `
-    <h1>Name: ${data.name}<h1>
-    `
-  console.log(pokeData)
-  dataContainer.insertAdjacentHTML('beforeend', pokeData)
+
+
+async function fetchPokeList() {
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=100&offset=51`
+  
+  try {
+    const listResponse = await axios.get(url)
+    console.log(listResponse)
+    let listData = listResponse.data.results
+    console.log(listData)
+    // show data here?
+    return listResponse
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-showPokeData()
+// fetchPokeList()
+
+
+/* ==========================================================================
+   Call API Data and Display to DOM 
+========================================================================== */
+
+// Grab data from API and attach info to left container screen
+
+const showPokeData = (data) => {  
+  
+  // console.log("inside funct", data)
+    leftContainer = document.querySelector('.left-container-screen')
+    console.log(leftContainer)
+
+    const pokeData = `
+      <h1>Name: ${data.name}<h1>
+      <img src=${data.sprites.front_default}>
+      <img src=${data.sprites.back_default}>
+      <div class="details">
+        <span>Height: ${data.height}</span>
+        <span>Weight: ${data.weight}</span>
+        <span>Abilities: ${data.moves[0].move}
+      </div>
+      `
+    console.log(pokeData)
+    leftContainer.insertAdjacentHTML('beforeend', pokeData)
+}
+
+/* ==========================================================================
+  Add Event Listeners
+========================================================================== */
+
+// Search Field in right container
+
+const form = document.querySelector('form')
+// console.log(form)
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const inputValue = document.querySelector('#pokemon-search').value
+  // console.log(inputValue)
+  removePokemon()
+  fetchData(inputValue)
+  document.querySelector('#pokemon-search').value = ""
+})
+
+// Remove previous search history
+
+function removePokemon() {
+    const leftContainer = document.querySelector('.left-container-screen')
+    console.log(leftContainer)
+    while (leftContainer.lastChild) {
+      leftContainer.removeChild(leftContainer.lastChild)
+    }
+  }
+
+// List in right container 
+
+// const rightList = document.querySelector('.right-screen-top')
+// console.log(rightList)
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const inputValue = document.querySelector('#pokemon-search').value
+//   // console.log(inputValue)
+//   removePokemon()
+//   fetchData(inputValue)
+//   document.querySelector('#pokemon-search').value = ""
+// })
+
+
+
+/* ==========================================================================
+  Generate List for Right Container
+========================================================================== */
+
+
+
+function app = {
+  const pokelist = getPokeList()  //return array of Pokemon []
+  const paginatedPokeList = paginatingPokeList(pokelist) //takes Poke list returns [[20], [20], [20], [20], [20]]
+  appendPokeToPage(paginatedPokeList) //takes paginated Poke list
+  const inputValue = getInputValue()
+  
+  buttonSetup(searchIndividualPokeAPICall, inputValue) //attach event listener to button  
+}
+
