f133ece054311b4bcc0e2f595b661aacf91882d1
Jeronimo Maranion <jeronimomaranion@MacBook-Pro-2.lan1>
2021-03-01 10:03:11 -0500

    90% done

--- app.js
+++ app.js
@@ -1,92 +1,141 @@
 /* ==========================================================================
+   Dom 
+========================================================================== */
+
+const pokeListItems = document.querySelectorAll('.list-item');
+const leftContainer = document.querySelector('.main-screen hide')
+const pokeName = document.querySelector('.poke-name');
+const pokeId = document.querySelector('.poke-id');
+const pokeFrontImage = document.querySelector('.poke-front-image');
+const pokeBackImage = document.querySelector('.poke-back-image');
+const pokeWeight = document.querySelector('.poke-weight');
+const pokeHeight = document.querySelector('.poke-height');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+
+
+/* ==========================================================================
    Grab API 
 ========================================================================== */
 
-// Grab individual Pokemon data 
-
-async function fetchData(pokemon) {
+async function fetchPokeData(pokemon) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   
   try {
     const response = await axios.get(url)
-    // console.log(response)
+    console.log(response)
     let data = response.data
     
-    showPokeData(data)
+    pokeName.textContent = data['name'];
+    pokeId.textContent = '#' + data['id']
+    pokeWeight.textContent = data['weight'];
+    pokeHeight.textContent = data['height'];
+    pokeFrontImage.src = data['sprites']['front_default'] || '';
+    pokeBackImage.src = data['sprites']['back_default'] || '';
+    
     return response
   } catch (err) {
     console.error(err)
   }
 }
 
-// fetchData('pikachu')
+// fetchData('bulbasaur')
 
 // Grab entire list of Pokemon 
 
+let prevUrl = null;
+let nextUrl = null;
+
+const fetchPokeList = url => {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const { results, previous, next } = data;
+      prevUrl = previous;
+      nextUrl = next;
+
+      for (let i = 0; i < pokeListItems.length ; i++) {
+        const pokeListItem = pokeListItems[i];
+        const resultData = results[i];
+
+        if (resultData) {
+          const { name, url } = resultData;
+          const urlArray = url.split('/');
+          const id = urlArray[urlArray.length - 2];
+          pokeListItem.textContent = id + '. ' + (name);
+        } else {
+          pokeListItem.textContent = '';
+        }
+      }
+    });
+};
+
+// Referenced to video https://www.youtube.com/watch?v=tFVdxGgJPCo for tips on fetchPokeList
 
 
 
-async function fetchPokeList() {
-  const url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0`
-  
-  try {
-    const listResponse = await axios.get(url)
-    console.log(listResponse)
-    let pokeArray = listResponse.data.results
-    console.log(pokeArray)
-    appendPokeList(pokeArray) // invoking appendPokeList function
 
-    // show data here?
-    return listResponse
-  } catch (err) {
-    console.error(err)
+
+const handleLeftButtonClick = () => {
+  if (prevUrl) {
+    fetchPokeList(prevUrl);
   }
+};
+
+const handleRightButtonClick = () => {
+  if (nextUrl) {
+    fetchPokeList(nextUrl);
+  }
+};
+
+const handleListItemClick = (e) => {
+  if (!e.target) return;
+
+  const listItem = e.target;
+  if (!listItem.textContent) return;
+
+  const id = listItem.textContent.split('.')[0];
+  fetchPokeData(id);
+};
+
+
+// adding event listeners
+leftButton.addEventListener('click', handleLeftButtonClick);
+rightButton.addEventListener('click', handleRightButtonClick);
+for (const pokeListItem of pokeListItems) {
+  pokeListItem.addEventListener('click', handleListItemClick);
 }
 
-fetchPokeList()
 
-function appendPokeList(pokeArray) {
-  const pokeList = document.querySelector('.right-screen-top')
-  pokeArray.forEach((pokemon) => {
-    const pokeName = document.createElement('p')
-    
-    pokeName.textContent = pokemon.name
-    console.log(pokeName)
-    console.log(pokemon.name)
-    pokeList.appendChild(pokeName)
-  }) 
-}
+// initialize App
+fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
 
 
-
-
-/* ==========================================================================
-   Call API Data and Display to DOM 
-========================================================================== */
-
-// Grab data from API and attach info to left container screen
-
-
-
-const showPokeData = (data) => {  
+// const showPokeData = (data) => {  
   
-  // console.log("inside funct", data)
-    const leftContainer = document.querySelector('.left-container-screen')
-    console.log(leftContainer)
+//   console.log("inside funct", data)
+    
+//   console.log(leftContainer)
 
-    const pokeData = `
-      <h1>Name: ${data.name}<h1>
-      <img src=${data.sprites.front_default}>
-      <img src=${data.sprites.back_default}>
-      <div class="details">
-        <span>Height: ${data.height}</span>
-        <span>Weight: ${data.weight}</span>
-        <span>Abilities: ${data.moves[0].move}
-      </div>
-      `
-    // console.log(pokeData)
-    leftContainer.insertAdjacentHTML('beforeend', pokeData)
-}
+//     // const pokeData = `
+//   pokeName.textContent = capitalize(data['name']);
+//   pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+//   pokeWeight.textContent = data['weight'];
+//   pokeHeight.textContent = data['height'];
+//   pokeFrontImage.src = data['sprites']['front_default'] || '';
+//   pokeBackImage.src = data['sprites']['back_default'] || '';
+//       <h1 class="poke-name"> Name: ${data.name}<h1>
+//       <img class="poke-front-image" src=${data.sprites.front_default}>
+//       <img class="poke-back-image" src=${data.sprites.back_default}>
+//       <div class="details">
+//         <span>Height: ${data.height}</span>
+//         <span>Weight: ${data.weight}</span>
+//         <span>Index #: ${data.id}</span>
+//       </div>
+//       `
+//     console.log(pokeData)
+//     leftContainer.insertAdjacentHTML('beforeend', pokeData)
+// }
 
 
 /* ==========================================================================
@@ -95,32 +144,8 @@ const showPokeData = (data) => {
 
 // Search Field in right container
 
-const form = document.querySelector('form')
-// console.log(form)
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const inputValue = document.querySelector('#pokemon-search').value
-  // console.log(inputValue)
-  removePokemon()
-  fetchData(inputValue)
-  document.querySelector('#pokemon-search').value = ""
-})
-
-// Remove previous search history
-
-function removePokemon() {
-    const leftContainer = document.querySelector('.left-container-screen')
-    console.log(leftContainer)
-    while (leftContainer.lastChild) {
-      leftContainer.removeChild(leftContainer.lastChild)
-    }
-  }
-
-// List in right container 
-
-// const rightList = document.querySelector('.right-screen-top')
-// console.log(rightList)
+// const form = document.querySelector('form')
+// // console.log(form)
 
 // form.addEventListener('submit', (e) => {
 //   e.preventDefault()
@@ -131,11 +156,62 @@ function removePokemon() {
 //   document.querySelector('#pokemon-search').value = ""
 // })
 
+// const pTags = document.querySelectorAll('p')
+// pTags.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   const inputValue = document.querySelectorAll('p').value
+//   // console.log(inputValue)
+//   // removePokemon()
+//   fetchData(inputValue)
+//   document.querySelector('#pokemon-search').value = ""
+  
+// })
+
+// console.log(pTags)
+// const listItemClick = (e) => {
+//   if (!e.target) return;
+
+//   const listItem = e.target;
+//   if (!listItem.textContent) return;
+//   console.log(listItem.textContent)
+
+//   const pokemon = listItem.textContent.split('.')[0];
+//   fetchData(pokemon);
+
+//   for (const pTag of pTags) {
+//     pTags.addEventListener('click', listItemClick);
+//   }
+// }
+// const leftButton = document.querySelector('.left-button');
+// const rightButton = document.querySelector('.right-button');
+// let prevUrl = null;
+// let nextUrl = null;
+
+// const rightButtonClick = () => {
+//   if (nextUrl) {
+//     fetchPokeList(nextUrl);
+//   }
+// };
+
+// // leftButtonClick.addEventListener('click', handleLeftButtonClick);
+// rightButton.addEventListener('click', rightButtonClick);
 
 
-/* ==========================================================================
-  Generate List for Right Container
-========================================================================== */
+// // Remove previous search history
+
+// function removePokemon() {
+//   console.log(leftContainer)
+
+//   while (leftContainer.lastChild) {
+//     leftContainer.removeChild(leftContainer.lastChild)
+//   }
+// }
+    
+
+
+
+
+
 
 
 
